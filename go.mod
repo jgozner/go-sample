64f6f8d978f26cdbb948bc8b9862e7bae52d9bdb
jgozner <jgozner@outlook.com>
2024-02-14 11:39:03 -0800

    Added Go sample

---
+++ go.mod
@@ -0,0 +1,5 @@
+module sample/m
+
+go 1.22.0
+
+require github.com/pdftron/pdftron-go/v2 v2.2.7
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    . "github.com/pdftron/pdftron-go/v2"
+)
+
+func main() {
+    PDFNetInitialize("key");
+}
